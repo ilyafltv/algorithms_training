7d5c4ff6b53cc99058edf34b12fbbcd01994c16d
ilya <keebro30@gmail.com>
2023-10-26 01:21:00 +0300

    string algorithms was updated

--- strings.js
+++ strings.js
@@ -2,27 +2,19 @@
 
 // Для проверки работоспособности функций раскомментируйте вызов соответствующей функции и запустите файл
 
-// 1. Функция подсчёта количества слов(без цифр, небуквенных символов и пробелов) в строке 
+// 1. Функция подсчёта количества слов в строке 
 
 const wordCountFunction = (str) => {
-    let lastLetter, isLetter, wordsCount = 0;
-    for (let i = 0; i < str.length; i++) {
-        lastLetter = isLetter;
-        isLetter = (str[i].match(/[А-Я,а-я,A-Z,a-z]/gi)) ? true : false;
-        wordsCount = (!lastLetter && isLetter) ? ++wordsCount : wordsCount;
-    }
-    return wordsCount;
+    return str.trim().split(/[\s]{1,}/g).length;
 }
 
-// console.log(`В строке ${wordCountFunction(" В этой строке 10 слов, несмотря на знаки и  ЛИШНИЕ  пробелы! ")} слов`);
-// console.log(`В строке ${wordCountFunction("You can also use latin alphabet(6).")} слов`);
+// console.log(wordCountFunction(" В этой строке одиннадцать слов, несмотря на знаки и  ЛИШНИЕ  пробелы! "));
+// console.log(wordCountFunction("You can also use latin alphabet(6)."));
 
 // 2. Функция подсчёта прописных букв в строке
 
 const upperCaseCheckFunction = (str) => {
-    let sum = 0;
-    for (let i = 0; i < str.length; i++) sum = (str[i] === str[i].toUpperCase() && str[i].match(/[А-Я,A-Z]/gi)) ? ++sum : sum;
-    return sum;
+    return str.match(/[А-Я,A-Z]/g).length;
 }
 
 // console.log(upperCaseCheckFunction('Подсчет Прописных Букв В Строке(5)'));
@@ -38,39 +30,22 @@ const deleteDuplicateFuntion = (str) => {
 // console.log(deleteDuplicateFuntion("aaabbbcccabcAAA"));
 // console.log(deleteDuplicateFuntion("AaAaAa00bcDE11"));
 
-// 4. Функция поиска самого длинного слова(первого) и подсчета количества его символов
+// 4. Функция подсчета количества символов самого длинного слова
 
 const findTheLongestWord = (str) => {
-    let isLetter, word, longestWord = '', maxCount = 0, lettersCount = 0;
-    for (let i = 0; i < str.length; i++) {
-        isLetter = (str[i].match(/[А-Я,а-я,A-Z,a-z]/gi)) ? true : false;
-        lettersCount = isLetter ? ++lettersCount : 0;
-        word = isLetter ? word + str[i] : '';
-        maxCount = lettersCount > maxCount ? lettersCount : maxCount;
-        if (word.length == maxCount && longestWord.length < word.length) longestWord = word;
-    }
-    console.log(`Слово: ${longestWord}, Количество символов: ${maxCount}`);
+    const arr = str.replace(/[.,?!]{1,}/g, ' ').trim().split(/[\s]{1,}/);
+    return arr.reduce((a, x) => a = (a > x.length) ? a : x.length, 0);
 }
 
-// findTheLongestWord('Один, Два, Три, Четыре');
-// findTheLongestWord('What is the longest word here?');
+// console.log(findTheLongestWord('Один, Два, Три, Четыре'));
+// console.log(findTheLongestWord('the longest word...?'));
 
 // 5. Функция поиска всех слов в строке, начинающихся с определенной буквы(любого регистра)
 
 const findWords = (str, x) => {
-    let isLetter = false, lastLetter, wordX = '', wordXDetected;
-    for (let i = 0; i < str.length; i++) {
-        lastLetter = isLetter;
-        isLetter = (str[i].match(/[А-Я,а-я,A-Z,a-z]/gi)) ? true : false;
-        wordXDetected = !isLetter ? false : wordXDetected;                                                      // False, если не буква
-        wordXDetected = (str[i].toUpperCase() === x && lastLetter == false && isLetter) ? true : wordXDetected; // True, если первая буква совпала с искомой
-        wordX = isLetter && wordXDetected ? wordX + str[i] : wordX;
-        if ((isLetter == false && wordX !== '') || (wordX !== '' && i == str.length - 1)) {
-            console.log(wordX);
-            wordX = '';
-        }
-    }
+    const arr = str.replace(/[.,?!]{1,}/g, ' ').trim().split(/[\s]{1,}/);
+    return arr.filter(y => y[0].toLowerCase().match(x.toLowerCase()));
 }
 
-// findWords('арбуз Дыня Ананас яблоко Огурец Авокадо', 'А');
-// findWords('арбуз Дыня Ананас яблоко Огурец Авокадо', 'О');
+// console.log(findWords('арбуз Дыня Ананас яблоко Огурец Авокадо', 'А'));
+// console.log(findWords('арбуз Дыня Ананас яблоко Огурец Авокадо', 'О'));
